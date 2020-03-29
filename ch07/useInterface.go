9f3386267d03bb01da7da40e1919f772d3ec54c8
banoris <banoris@protonmail.com>
2020-03-29 09:52:36 +0200

    Fix compile error + play around with Go interface

--- ch07/useInterface.go
+++ ch07/useInterface.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
-	"myInterface"
+	"./myInterface" // relative import path - bad practice
 )
 
 type square struct {
@@ -14,6 +14,10 @@ type circle struct {
 	R float64
 }
 
+// Area() is a behavior defined in Shape intf
+// Here, square type implements this Area behavior
+// using the Type method syntax. A type method is a
+// function with 'receiver' as an argument
 func (s square) Area() float64 {
 	return s.X * s.X
 }
@@ -26,10 +30,17 @@ func (s circle) Area() float64 {
 	return s.R * s.R * math.Pi
 }
 
+// Try to remove this. Compiler will complain that
+// 'circle' doesn't is not a Shape, i.e., Perimeter()
+// is undefined
 func (s circle) Perimeter() float64 {
 	return 2 * s.R * math.Pi
 }
 
+// Here is the cool part. You don't need to have
+// a Calculate function each for {square, circle, rect}
+// We know that object that implements the Shape intf
+// have Area() and Perimeter() defined.
 func Calculate(x myInterface.Shape) {
 	_, ok := x.(circle)
 	if ok {
